d454cadc05a70327a0ef8576655e6fd01369525e
Jeffrey C. Lerman <Jeffrey.Lerman@gmail.com>
2024-12-05 14:02:45 -0800

    AIE-3321 fix importlib resources deprecation warnings (#19)
    * use new files() interface now preferred by importlib.resources
    
    * minor linting fix

--- philter_lite/filters/filter_db.py
+++ philter_lite/filters/filter_db.py
@@ -1,5 +1,5 @@
 import sys
-from importlib import resources
+from importlib.resources import as_file, files
 from typing import Any, MutableMapping
 
 if sys.version_info < (3, 11):
@@ -7,20 +7,27 @@ if sys.version_info < (3, 11):
 else:
     import tomllib
 
-
 from philter_lite import filters
 
 
+def _get_toml_from_filters(filename: str) -> MutableMapping[str, Any]:
+    source = files(filters).joinpath(filename)
+    with as_file(source) as path:
+        with open(path, "rb") as toml_file:
+            toml_data = tomllib.load(toml_file)
+    return toml_data
+
+
 def load_regex_db() -> MutableMapping[str, Any]:
-    return tomllib.loads(resources.read_text(filters, "regex.toml"))
+    return _get_toml_from_filters("regex.toml")
 
 
 def load_regex_context_db() -> MutableMapping[str, Any]:
-    return tomllib.loads(resources.read_text(filters, "regex_context.toml"))
+    return _get_toml_from_filters("regex_context.toml")
 
 
 def load_set_db() -> MutableMapping[str, Any]:
-    return tomllib.loads(resources.read_text(filters, "set.toml"))
+    return _get_toml_from_filters("set.toml")
 
 
 regex_db: MutableMapping[str, Any] = load_regex_db()
